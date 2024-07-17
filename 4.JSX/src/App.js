87078038a98bb6f660a4a3277dfeeb4ca934ad84
Shrutik <satheshrutik4141@gmail.com>
2024-07-17 16:53:12 +0530

    jsx Example Demo

---
+++ 4.JSX/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import DemoJSX from './DemoJSX';
+
+
+function App() {
+  return (
+    <div>
+    <h1> HELLO</h1>
+    <DemoJSX />
+  
+   </div>
+  );
+}
+
+export default App;
---
+++ 4.JSX/src/DemoJSX.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const DemoJSX = () => {
+ 
+     const name="Shrutik";
+
+  return (
+    <div>
+    <div>DemoJSX</div>
+    <div>My Name is {name}</div>
+    <div> the Addition of 5 + 5 is {5 + 5}</div>
+    </div>
+  )
+}
+
+export default DemoJSX
